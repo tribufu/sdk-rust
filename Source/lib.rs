6851484a3f720df1f2f90eaf50f07de171d752e5
Werner <guilherme.werner@tribufu.com>
2023-05-15 18:42:12 -0300

    Update project

--- Source/lib.rs
+++ Source/lib.rs
@@ -1,11 +1,5 @@
 // Copyright (c) TribuFu. All Rights Reserved
 
-//! TribuFu SDK.
-
 #![allow(non_snake_case)]
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-pub fn Hello(input: String) -> String {
-    return format!("Hello {}", input);
-}
