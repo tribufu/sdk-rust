7b3af5d2919366a7e36a839ea224e743168eec92
Guilherme Werner <guilhermewerner@tribufu.com>
2025-06-11 12:49:58 -0300

    Update build.ps1

--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -9,4 +9,4 @@ Remove-Item -Path "bin/win-x64/*" -Force -Recurse
 Copy-Item -Path "target/debug/tribufu_sdk.dll.lib" -Destination "bin/win-x64/tribufu_sdk.lib"
 Copy-Item -Path "target/debug/tribufu_sdk.dll" -Destination "bin/win-x64/tribufu_sdk.dll"
 
-msbuild /p:Configuration="debug" /p:Platform="win-x64"
+#msbuild /p:Configuration="debug" /p:Platform="win-x64"
